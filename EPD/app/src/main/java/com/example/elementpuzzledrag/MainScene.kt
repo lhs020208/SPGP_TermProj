da6d47ec60a25bde116384040be5ad6f9be8b75f
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-04-15 02:02:41 +0900

    MainScene 생성

---
+++ EPD/app/src/main/java/com/example/elementpuzzledrag/MainScene.kt
@@ -0,0 +1,8 @@
+package com.example.elementpuzzledrag
+
+import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
+import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+
+class MainScene(gctx: GameContext) : Scene(gctx) {
+
+}
