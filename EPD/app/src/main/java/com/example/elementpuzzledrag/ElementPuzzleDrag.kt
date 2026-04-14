a7ae77b9e19cb8b6759986d77b011c5a4d3a50e0
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-04-15 04:41:39 +0900

    리소스 이름과 폴더 정리, 그리드 생성

--- EPD/app/src/main/java/com/example/elementpuzzledrag/ElementPuzzleDrag.kt
+++ EPD/app/src/main/java/com/example/elementpuzzledrag/ElementPuzzleDrag.kt
@@ -9,8 +9,13 @@ import kr.ac.tukorea.ge.spgp2026.a2dg.activity.BaseGameActivity
 import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
 import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
 
-class ElementPuzzleDrag: BaseGameActivity() {
+class ElementPuzzleDrag : BaseGameActivity() {
+    override val drawsDebugGrid: Boolean = false
+    override val drawsDebugInfo: Boolean = false
+    override val drawsFpsGraph: Boolean = false
+
     override fun createRootScene(gctx: GameContext): Scene {
+        gctx.metrics.setSize(900f, 1600f)
         return MainScene(gctx)
     }
 }
